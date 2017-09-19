916840b3d8a845bb9d7b6194544521cf4506d49b
Magnus Gustafsson <magnus.gustafsson@king.com>
2017-09-19 13:55:59 +0200

    Made the metric fields public

--- src/main/java/net/spy/memcached/MemcachedConnection.java
+++ src/main/java/net/spy/memcached/MemcachedConnection.java
@@ -109,25 +109,25 @@ public class MemcachedConnection extends SpyThread {
    */
   private static final int MAX_CLONE_COUNT = 100;
 
-  private static final String RECON_QUEUE_METRIC =
+  public static final String RECON_QUEUE_METRIC =
     "[MEM] Reconnecting Nodes (ReconnectQueue)";
-  private static final String SHUTD_QUEUE_METRIC =
+  public static final String SHUTD_QUEUE_METRIC =
     "[MEM] Shutting Down Nodes (NodesToShutdown)";
-  private static final String OVERALL_REQUEST_METRIC =
+  public static final String OVERALL_REQUEST_METRIC =
     "[MEM] Request Rate: All";
-  private static final String OVERALL_AVG_BYTES_WRITE_METRIC =
+  public static final String OVERALL_AVG_BYTES_WRITE_METRIC =
     "[MEM] Average Bytes written to OS per write";
-  private static final String OVERALL_AVG_BYTES_READ_METRIC =
+  public static final String OVERALL_AVG_BYTES_READ_METRIC =
     "[MEM] Average Bytes read from OS per read";
-  private static final String OVERALL_AVG_TIME_ON_WIRE_METRIC =
+  public static final String OVERALL_AVG_TIME_ON_WIRE_METRIC =
     "[MEM] Average Time on wire for operations (µs)";
-  private static final String OVERALL_RESPONSE_METRIC =
+  public static final String OVERALL_RESPONSE_METRIC =
     "[MEM] Response Rate: All (Failure + Success + Retry)";
-  private static final String OVERALL_RESPONSE_RETRY_METRIC =
+  public static final String OVERALL_RESPONSE_RETRY_METRIC =
     "[MEM] Response Rate: Retry";
-  private static final String OVERALL_RESPONSE_FAIL_METRIC =
+  public static final String OVERALL_RESPONSE_FAIL_METRIC =
     "[MEM] Response Rate: Failure";
-  private static final String OVERALL_RESPONSE_SUCC_METRIC =
+  public static final String OVERALL_RESPONSE_SUCC_METRIC =
     "[MEM] Response Rate: Success";
 
   /**
